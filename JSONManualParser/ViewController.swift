de8669ef2496bb7f6ba6e11dcfbac2d534d7ef40
slambeable <41022916+slambeable@users.noreply.github.com>
2022-01-27 11:08:53 +0300

    Initial Commit

---
+++ JSONManualParser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONManualParser
+//
+//  Created by Андрей Евдокимов on 27.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
